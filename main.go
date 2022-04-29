72f2d2807e23026e2b45eabbeea7ea2378e1505d
LeGamerDc <451954040@qq.com>
2022-04-29 14:20:36 +0800

    make fmt

--- main.go
+++ main.go
@@ -1,22 +1,3 @@
 package main
 
-import (
-	"fmt"
-	"unsafe"
-)
-
-const (
-	pageStructSize = 16 * 1024 // 16KB
-	pageSize       = int(pageStructSize - unsafe.Sizeof(int64(0)) - unsafe.Sizeof(0))
-)
-
-type page struct {
-	cnt int64
-	off int
-	buf [pageSize]byte
-}
-
-func main() {
-	var p = page{}
-	fmt.Println(unsafe.Sizeof(p))
-}
+func main() {}
